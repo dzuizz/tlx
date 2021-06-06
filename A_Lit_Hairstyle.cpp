40ff058842e46903d8bdd9756e3f83a383569334
dzuizz <59071340+dzuizz@users.noreply.github.com>
2021-06-06 15:37:10 +0800

    Solved Lit Hairstyle

---
+++ A_Lit_Hairstyle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double n, d;
+    cin >> n >> d;
+    if (d == 1)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        cout << "YES";
+    }
+}
