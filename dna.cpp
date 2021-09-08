7e5ead299175841f39fbc7eabc981f2d1c34b037
dzuizz <59071340+dzuizz@users.noreply.github.com>
2021-09-08 18:39:22 +0800

    Solved Mutating Dna (100 points)

--- dna.cpp
+++ dna.cpp
@@ -1,50 +1,67 @@
 #include "dna.h"
+#include <algorithm>
 
-std::string s1, s2;
+int l_pairs[100000][6]; // AT, TA, AC, CA, CT, TC
 void init(std::string a, std::string b) {
-	s1=a, s2=b;
+	//pairs
+	if(a[0]=='A'){
+		if(b[0]=='T') l_pairs[0][0]++;
+		else if(b[0]=='C') l_pairs[0][2]++;
+	}else if(a[0]=='T'){
+		if(b[0]=='A') l_pairs[0][1]++;
+		else if(b[0]=='C') l_pairs[0][5]++;
+	}else{
+		if(b[0]=='A') l_pairs[0][3]++;
+		else if(b[0]=='T') l_pairs[0][4]++;
+	}
+	for(int i=1;i<a.length();i++){
+		for(int j=0;j<6;j++) l_pairs[i][j] = l_pairs[i-1][j];
+		if(a[i]=='A'){
+			if(b[i]=='T') l_pairs[i][0]++;
+			else if(b[i]=='C') l_pairs[i][2]++;
+		}else if(a[i]=='T'){
+			if(b[i]=='A') l_pairs[i][1]++;
+			else if(b[i]=='C') l_pairs[i][5]++;
+		}else{
+			if(b[i]=='A') l_pairs[i][3]++;
+			else if(b[i]=='T') l_pairs[i][4]++;
+		}
+	}
 }
 
 int get_distance(int x, int y) {
-	int need_to_mutate = 0;
-	std::string sub_a = s1.substr(x, y-x+1), sub_b = s2.substr(x, y-x+1);
-	int count_sub_a[3] = {0, 0, 0}, count_sub_b[3] = {0, 0, 0};
-	for(int i=0;i<y-x+1;i++){
-		switch (sub_a[i]){
-		case 'A':
-			count_sub_a[0] += 1;
-			break;
-		case 'T':
-			count_sub_a[1] += 1;
-			break;
-		case 'C':
-			count_sub_a[2] += 1;
-			break;
-		default:
-			break;
-		}
-		switch (sub_b[i]){
-		case 'A':
-			count_sub_b[0] += 1;
-			break;
-		case 'T':
-			count_sub_b[1] += 1;
-			break;
-		case 'C':
-			count_sub_b[2] += 1;
-			break;
-		default:
-			break;
-		}
-	}
-	if(count_sub_a[0]==count_sub_b[0] && count_sub_a[1]==count_sub_b[1] && count_sub_a[2]==count_sub_b[2]){
-		for(int i=0;i<y-x+1;i++){
-			if(sub_a[i]!=sub_b[i]){
-				need_to_mutate++;
+	int mutations = 0, rem=0;
+	int nAT = l_pairs[y][0], nTA = l_pairs[y][1];
+	int nAC = l_pairs[y][2], nCA = l_pairs[y][3];
+	int nCT = l_pairs[y][4], nTC = l_pairs[y][5];
+	int pairs[6];
+	if(x>0){
+		nAT -= l_pairs[x-1][0], nTA -= l_pairs[x-1][1];
+		nAC -= l_pairs[x-1][2], nCA -= l_pairs[x-1][3];
+		nCT -= l_pairs[x-1][4], nTC -= l_pairs[x-1][5];
+		if((nAT + nAC == nTA + nCA) && (nTA + nTC == nAT + nCT) && (nCA + nCT == nAC + nTC)){
+			for(int i=0;i<6;i++) pairs[i] = l_pairs[y][i] - l_pairs[x-1][i];
+			for(int i=0;i<6;i+=2){
+				int smaller = std::min(pairs[i], pairs[i+1]);
+				mutations += smaller;
+				pairs[i] -= smaller;
+				pairs[i+1] -= smaller;
+				rem += pairs[i] + pairs[i+1];
 			}
-		}
-		return (need_to_mutate+need_to_mutate%2)/2;
+			mutations += rem/3*2;
+		}else return -1;
 	}else{
-		return -1;
+		if((nAT + nAC == nTA + nCA) && (nTA + nTC == nAT + nCT) && (nCA + nCT == nAC + nTC)){
+			for(int i=0;i<6;i++) pairs[i] = l_pairs[y][i];
+			for(int i=0;i<6;i+=2){
+				int smaller = std::min(pairs[i], pairs[i+1]);
+				mutations += smaller;
+				pairs[i] -= smaller;
+				pairs[i+1] -= smaller;
+				rem += pairs[i] + pairs[i+1];
+			}
+			mutations += rem/3*2;
+		}else return -1;
 	}
+	return mutations;
 }
